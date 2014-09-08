bbed391c4dd2b4af5fddf3111c200954ab0891a8
Florian Moreno <fr.florianmoreno@gmail.com>
2014-09-08 14:42:06 +0200

    Add french translation

---
+++ languages/fr.php
@@ -0,0 +1,45 @@
+<?php
+/**
+Copyright (c) 2014, Revoplay.de Development
+http://www.revoplay.de
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#define('_logout', 'Déconnexion');
+define('_id_dont_exist', 'L\'identifiant saisi n\'existe pas !');
+define('_login_fail', 'Erreur lors de la connexion');
+define('_error', 'Erreur');
+define('_notice', 'Information');
+define('_success', 'Succès');
+define('_warning', 'Attention');
+
+## Groupe ##
+define('_group_guest', 'Invité');
+define('_group_admin', 'Administrateur');
+define('_group_member', 'Mitglied');
+define('_group_moderator', 'Modérateur');
+define('_group_user', 'Utilisateur');
+
+## Navigation ##
+define('_home', 'Accueil');
